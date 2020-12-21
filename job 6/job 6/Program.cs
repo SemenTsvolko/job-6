92738a9a3fda5ded625db3c45eab25b9c090789f
SemenTsvolko <75906654+SemenTsvolko@users.noreply.github.com>
2020-12-21 14:46:42 +0300

    Add files via upload

---
+++ job 6/job 6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace job_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Базовый уровень
+            Console.WriteLine("Введите число n ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= x; i++)
+            {
+                if ((x % i) == 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
